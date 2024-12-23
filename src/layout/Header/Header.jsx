4cfb839a8a238c1718299894f1302eb055fd1112
sudac_tudak <30slava09@mail.ru>
2024-12-23 13:49:51 +0300

    adaptive MainLayout for laptops and mobile; create mobile burger menu; create useDisableScroll hook

--- src/layout/Header/Header.jsx
+++ src/layout/Header/Header.jsx
@@ -1,6 +1,45 @@
+import { useState } from 'react';
 import styles from './Header.module.scss';
 import cn from 'classnames';
+import { Sidebar } from '../Sidebar/Sidebar';
+import { Logo } from '../Logo/Logo';
+import { useDisableScroll } from '../../hooks/useDisableScroll';
 
 export const Header = ({ className }) => {
-  return <header className={cn(styles['header'], className)}>Header</header>;
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [disableScroll, enableScroll] = useDisableScroll();
+
+  const handleBurgerBtnClick = () => {
+    if (isMenuOpen) {
+      enableScroll();
+    } else {
+      disableScroll();
+    }
+    setIsMenuOpen((prevState) => !prevState);
+  };
+
+  return (
+    <header className={cn(styles['header'], className)}>
+      <Logo
+        className={cn(styles['header__logo'], {
+          [styles['is-menu-open']]: isMenuOpen
+        })}
+      />
+      <button
+        className={cn(styles['header__burger'], styles['icon-menu'], {
+          [styles['is-active']]: isMenuOpen
+        })}
+        onClick={handleBurgerBtnClick}
+      >
+        <span></span>
+      </button>
+      <div
+        className={cn(styles['header__menu'], styles['menu-header'], {
+          [styles['is-open']]: isMenuOpen
+        })}
+      >
+        <Sidebar />
+      </div>
+    </header>
+  );
 };
