01925301b4c00261badc895dd8a9914300fee332
sudac_tudak <30slava09@mail.ru>
2024-12-26 02:17:52 +0300

    change Layout styles; adaptive buttons and HomePage preview; add some styles on NotFoundPage

--- src/pages/NotFoundPage/NotFoundPage.tsx
+++ src/pages/NotFoundPage/NotFoundPage.tsx
@@ -1,5 +1,21 @@
+import { Link, useLocation } from 'react-router-dom';
+import { Heading } from '../../components/Heading/Heading';
 import styles from './NotFoundPage.module.scss';
+import { APP_ROUTES } from '../../routes';
 
 export const NotFoundPage = () => {
-  return <div>NotFoundPage</div>;
+  const location = useLocation();
+  return (
+    <div className={styles['not-found']}>
+      <Heading level="h1" className={styles['not-found__title']}>
+        Страница не найдена
+      </Heading>
+      <p className={styles['not-found__text']}>
+        Страница по адресу "{location.pathname}" не найдена
+      </p>
+      <Link to={APP_ROUTES.HOMEPAGE} className={styles['not-found__link']}>
+        На главную
+      </Link>
+    </div>
+  );
 };
